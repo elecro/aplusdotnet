f13d876f79e63fd3ee15fe8ce7f625104ee8ca62
Peter Gal <galpeter@inf.u-szeged.hu>
2015-11-05 01:26:02 +0100

    Add DllBuilder and DllConsumer solutions and remove the AplusWPF/IDE ones

---
+++ src/Aplus/DllBuilder/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AplusCore.Compiler;
+using AplusCore.Runtime;
+using Microsoft.Scripting.Runtime;
+using AplusCore.Types;
+
+using DLR = System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Emit;
+using Microsoft.Scripting;
+
+namespace DllBuilder
+{
+    class Program
+    {
+
+        static void BuildDll(string name, DLR.Expression<Func<Aplus, AType>> lambda, Aplus runtime)
+        {
+            Type[] types = lambda.Parameters.Select(param => param.Type).ToArray();
+
+            AssemblyName asmName = new AssemblyName(name + "Generated");
+            AssemblyBuilder asmBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(asmName, AssemblyBuilderAccess.RunAndSave);
+            ModuleBuilder moduleBuilder = asmBuilder.DefineDynamicModule(name + "Module", name + "Module.dll", true);
+            TypeBuilder typeBuilder = moduleBuilder.DefineType("AplusGenerated", TypeAttributes.Public);
+            MethodBuilder methodBuilder = typeBuilder.DefineMethod("test", MethodAttributes.Public | MethodAttributes.Static,
+                lambda.ReturnType, types);
+
+            lambda.CompileToMethod(methodBuilder);
+
+            Type t = typeBuilder.CreateType();
+            asmBuilder.Save(name + "Generated.dll");
+
+            object result = t.GetMethod("test").Invoke(null, new object[] { runtime });
+
+            Console.WriteLine(result);
+        }
+
+        static void Main(string[] args)
+        {
+            Scope scope = new Scope();
+            scope.Storage["."] = new ScopeStorage();
+            scope.Storage["."]["a"] = AInteger.Create(109);
+            Aplus runtime = new Aplus(scope, LexerMode.ASCII);
+            runtime.Context = scope;
+
+            string src = @"
+                method{c}: c+1
+
+                method{a}";
+
+            DLR.Expression<Func<Aplus, AType>> lambda = AplusScriptCode.ParseToLambda(runtime, src);
+
+            BuildDll("Sum", lambda, runtime);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/Aplus/DllConsumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AplusCore.Runtime;
+using Microsoft.Scripting.Runtime;
+using AplusCore.Compiler;
+using AplusCore.Types;
+using Microsoft.Scripting;
+
+namespace DllConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scope scope = new Scope();
+            scope.Storage["."] = new ScopeStorage();
+            scope.Storage["."]["a"] = AInteger.Create(103);
+
+            Aplus runtime = new Aplus(scope, LexerMode.ASCII);
+            runtime.Context = scope;
+            AType result = AplusGenerated.test(runtime);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
