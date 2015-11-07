c3ad2e9ebde034011267075a2324830fbd980a0e
Peter Gal <galpeter@inf.u-szeged.hu>
2015-11-07 03:37:24 +0100

    Add DLLCompiler project
    This is now capable of generating dlls from a given
    A+ source. Currently it generates two classes, one static and one
    which should be instantiniated before using it.

---
+++ src/Aplus/AplusDllCompiler/AssemblyCreator.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+
+using DLR = System.Linq.Expressions;
+
+using AplusCore.Compiler;
+using AplusCore.Runtime;
+using AplusCore.Types;
+
+namespace AplusDllCompiler
+{
+    internal class AssemblyCreator
+    {
+        #region variables
+
+        private string assemblyBaseName;
+        private IDictionary<string, DLR.LambdaExpression> methods;
+
+        #endregion
+
+        #region properties
+
+        public string AssemblyBaseName { get { return this.assemblyBaseName; } }
+
+        public ICollection<string> MethodNames { get { return this.methods.Keys; } }
+
+        #endregion
+
+        #region constructor
+
+        public AssemblyCreator(string assemblyName)
+        {
+            this.methods = new Dictionary<string, DLR.LambdaExpression>();
+            this.assemblyBaseName = assemblyName;
+        }
+
+        #endregion
+
+        #region public methods
+
+        /// <summary>
+        /// Add an A+ method to be built into a DLL
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="method"></param>
+        public void AddMethod(string name, DLR.LambdaExpression method)
+        {
+            this.methods[name] = method;
+        }
+
+        /// <summary>
+        /// Builds a DLL based on the added methods and the assembly name specified
+        /// </summary>
+        /// <returns>Number of methods added to the generated DLL</returns>
+        public List<string> Build()
+        {
+            List<string> generatedNames = new List<string>();
+
+            AssemblyName asmName = new AssemblyName(this.assemblyBaseName + "Generated");
+            AssemblyBuilder asmBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(asmName, AssemblyBuilderAccess.Save);
+            ModuleBuilder moduleBuilder = asmBuilder.DefineDynamicModule(this.assemblyBaseName + "Module", this.assemblyBaseName + "Module.dll", true);
+            TypeBuilder staticTypeBuilder = moduleBuilder.DefineType(this.assemblyBaseName + "StaticClass", TypeAttributes.Public);
+
+            foreach (KeyValuePair<string, DLR.LambdaExpression> methodInfo in this.methods)
+            {
+                Type[] argumentTypes = methodInfo.Value.Parameters.Select(item => item.Type).ToArray();
+
+                // Create a static method
+                MethodBuilder staticMethodBuilder = staticTypeBuilder.DefineMethod(
+                    methodInfo.Key,
+                    MethodAttributes.Public | MethodAttributes.Static,
+                    methodInfo.Value.ReturnType,
+                    argumentTypes
+                );
+
+                methodInfo.Value.CompileToMethod(staticMethodBuilder);
+                string argNames = string.Join(", ", argumentTypes.Select(item => item.Name));
+                generatedNames.Add(string.Format("{0}.{1}({2})", staticTypeBuilder.Name, methodInfo.Key, argNames));
+            }
+
+            // Create a non-static mapping.
+            Type staticClass = staticTypeBuilder.CreateType();
+
+            TypeBuilder instanceTypeBuilder = moduleBuilder.DefineType(this.assemblyBaseName + "Class", TypeAttributes.Public);
+            // Create a private A+ runtime variable
+            string runtimeName = "_aplusRuntime";
+            FieldBuilder runtimeFieldBuilder = instanceTypeBuilder.DefineField(runtimeName, typeof(Aplus), FieldAttributes.Private);
+            generatedNames.Add(string.Format("{0}.{1}", instanceTypeBuilder.Name, runtimeName));
+
+            CreateConstructor(instanceTypeBuilder, runtimeFieldBuilder);
+            generatedNames.Add(string.Format("{0}..ctor()", instanceTypeBuilder.Name));
+            GenerateRuntimeGetter(instanceTypeBuilder, "Runtime", runtimeFieldBuilder);
+            generatedNames.Add(string.Format("{0}.Runtime", instanceTypeBuilder.Name));
+
+            foreach (KeyValuePair<string, DLR.LambdaExpression> methodInfo in this.methods)
+            {
+                GenerateInstanceCode(
+                    instanceTypeBuilder,
+                    methodInfo.Key,
+                    methodInfo.Value,
+                    runtimeFieldBuilder,
+                    staticClass.GetMethod(methodInfo.Key, BindingFlags.Public | BindingFlags.Static)
+                );
+                string argNames = string.Join(", ", methodInfo.Value.Parameters.Select(item => item.Type.Name));
+                generatedNames.Add(string.Format("{0}.{1}({2})", instanceTypeBuilder.Name, methodInfo.Key, argNames));
+            }
+
+            instanceTypeBuilder.CreateType();
+            asmBuilder.Save(asmName.Name + ".dll");
+
+            return generatedNames;
+        }
+
+        #endregion
+
+        #region type builder helpers
+
+        /// <summary>
+        /// Creates the Constructor code for the class.
+        /// </summary>
+        /// <param name="instanceTypeBuilder">Type on which the Constructor should be built.</param>
+        /// <param name="runtimeFieldBuilder">The FieldBuilder for the A+ runtime field.</param> 
+        private static void CreateConstructor(TypeBuilder instanceTypeBuilder, FieldBuilder runtimeFieldBuilder)
+        {
+            // Create a constuctor to initialize the A+ runtime
+            ConstructorBuilder constructorBuilder = instanceTypeBuilder.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard, new Type[] { });
+            ILGenerator constructorIl = constructorBuilder.GetILGenerator();
+            // Load 'this' & call the object's constructor
+            constructorIl.Emit(OpCodes.Ldarg_0);
+            constructorIl.Emit(OpCodes.Call, typeof(Object).GetConstructor(new Type[] { }));
+
+            // Load 'this'
+            constructorIl.Emit(OpCodes.Ldarg_0);
+
+            // Create a new Aplus object
+            constructorIl.Emit(OpCodes.Newobj, typeof(Aplus).GetConstructor(new Type[] { }));
+
+            // Store the new object into the A+ runtime variable
+            constructorIl.Emit(OpCodes.Stfld, runtimeFieldBuilder);
+            constructorIl.Emit(OpCodes.Ret);
+        }
+
+        /// <summary>
+        /// Generate a method to allow acces to the internal A+ runtime
+        /// </summary>
+        /// <param name="instanceTypeBuilder">The Type on which the method should be created</param>
+        /// <param name="name">Name of the method to generate</param>
+        /// <param name="runtimeFieldBuilder">Reference to the A+ runtime field</param>
+        private static void GenerateRuntimeGetter(TypeBuilder instanceTypeBuilder, string name, FieldBuilder runtimeFieldBuilder)
+        {
+            MethodBuilder getRuntimeBuilder = instanceTypeBuilder.DefineMethod("get_" + name, MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.SpecialName);
+            getRuntimeBuilder.SetReturnType(typeof(Aplus));
+            ILGenerator il = getRuntimeBuilder.GetILGenerator();
+            // Load 'this'
+            il.Emit(OpCodes.Ldarg_0);
+            // Load the value from the A+ runtime field
+            il.Emit(OpCodes.Ldfld, runtimeFieldBuilder);
+            // Return the value
+            il.Emit(OpCodes.Ret);
+
+            PropertyBuilder propBuilder = instanceTypeBuilder.DefineProperty(name, PropertyAttributes.None, typeof(Aplus), new Type[] { });
+            propBuilder.SetGetMethod(getRuntimeBuilder);
+        }
+
+        /// <summary>
+        /// Generate the 'forwarder' call to the static class's method
+        /// </summary>
+        /// <param name="typeBuiler"></param>
+        /// <param name="name"></param>
+        /// <param name="dlrMethod"></param>
+        /// <param name="runtimeFieldBuilder"></param>
+        /// <param name="targetMethod"></param>
+        private static void GenerateInstanceCode(TypeBuilder typeBuiler, string name, DLR.LambdaExpression dlrMethod, FieldBuilder runtimeFieldBuilder, MethodInfo targetMethod)
+        {
+            Type[] argumentTypes = dlrMethod.Parameters
+                    .Skip(1) // We'll skip the A+ runtime Argument now
+                    .Select(item => item.Type)
+                    .ToArray();
+
+            // && Create an instance method
+            MethodBuilder methodBuilder = typeBuiler.DefineMethod(
+                name,
+                MethodAttributes.Public,
+                dlrMethod.ReturnType,
+                argumentTypes
+            );
+
+            // Add names for paramters
+            // We start from 1 as the first is the A+ runtime
+            // and for the DefineParameter the 0 is the return paramter
+            for (int i = 1; i < dlrMethod.Parameters.Count; i++)
+            {
+                DLR.ParameterExpression parameter = dlrMethod.Parameters[i];
+                methodBuilder.DefineParameter(i, ParameterAttributes.None, parameter.Name);
+            }
+
+            // Build up static method call
+            ILGenerator il = methodBuilder.GetILGenerator();
+
+            // Load the static method
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldfld, runtimeFieldBuilder);
+
+            // Load the arguments for the static method
+            for (int arg = 0; arg < targetMethod.GetParameters().Length - 1; arg++)
+            {
+                il.Emit(OpCodes.Ldarg_S, arg + 1);
+            }
+
+            // Call & return
+            il.Emit(OpCodes.Call, targetMethod);
+            il.Emit(OpCodes.Ret);
+
+        }
+
+        #endregion
+    }
+}
---
+++ src/Aplus/AplusDllCompiler/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Scripting.Runtime;
+using Microsoft.Scripting;
+
+using DLR = System.Linq.Expressions;
+using DYN = System.Dynamic;
+
+using AplusCore.Compiler;
+using AplusCore.Runtime;
+using AplusCore.Types;
+using AplusCore.Compiler.AST;
+
+namespace AplusDllCompiler
+{
+    class Program
+    {
+        internal static Node ParseCode(string code)
+        {
+            FunctionInformation funcionInfo = new FunctionInformation(".");
+            return Parse.ASCIIString(code, funcionInfo);
+        }
+
+        internal static List<Node> FindUserDefMethods(Node treeTop)
+        {
+            List<Node> userMethods = new List<Node>();
+
+            // We expect that the top level is an ExpressionList type node
+            if (treeTop.NodeType != NodeTypes.ExpressionList)
+            {
+                return userMethods;
+            }
+
+            ExpressionList topExpressions = treeTop as ExpressionList;
+
+            foreach (Node expression in topExpressions.Items)
+            {
+                if (expression.NodeType == NodeTypes.UserDefFunction)
+                {
+                    userMethods.Add(expression);
+                }
+            }
+
+
+            return userMethods;
+        }
+
+        internal static List<DLR.LambdaExpression> ConvertNodes(List<Node> methods)
+        {
+            Aplus runtime = new Aplus();
+
+            AplusScope scope = new AplusScope(null, "__top__", runtime,
+                DLR.Expression.Parameter(typeof(Aplus), "__aplusRuntime__"),
+                DLR.Expression.Parameter(typeof(DYN.IDynamicMetaObjectProvider), "__module__")
+            );
+
+            List<DLR.LambdaExpression> dlrMethods = new List<DLR.LambdaExpression>();
+
+            foreach (Node userDefMethod in methods)
+            {
+                DLR.Expression varSet = userDefMethod.Generate(scope);
+
+                DLR.Expression wrappedLambda = ((DLR.MethodCallExpression)((DLR.UnaryExpression)varSet).Operand).Arguments.Last();
+                DLR.LambdaExpression lambda = (DLR.LambdaExpression)((DLR.MethodCallExpression)wrappedLambda).Arguments[1];
+
+                List<DLR.ParameterExpression> newParams = new List<DLR.ParameterExpression>();
+                newParams.Add(scope.GetRuntimeExpression());
+                // We must change the order of the AType arguments to have a correct C# call order
+                newParams.AddRange(lambda.Parameters.Skip(1).Reverse());
+
+                DLR.LambdaExpression resultingLambda =
+                    DLR.Expression.Lambda(
+                        DLR.Expression.Block(
+                            new DLR.ParameterExpression[] {
+                                scope.GetModuleExpression(),
+                                lambda.Parameters.First()
+                            },
+                            DLR.Expression.Assign(
+                                lambda.Parameters.First(),
+                                scope.GetRuntimeExpression()
+                            ),
+                            DLR.Expression.Assign(
+                                scope.GetModuleExpression(),
+                                DLR.Expression.PropertyOrField(scope.GetRuntimeExpression(), "Context")
+                            ),
+                            lambda.Body
+                        ),
+                        lambda.Name,
+                        newParams
+                    );
+
+                dlrMethods.Add(resultingLambda);
+            }
+
+            return dlrMethods;
+        }
+
+        static void Main(string[] args)
+        {
+            string src = @"
+                method{c}: { .d:=2; c+1 }
+                method2{c; e}: { (c * 0) + e + d }
+            ";
+
+            Node tree = ParseCode(src);
+            List<Node> methods = FindUserDefMethods(tree);
+            List<DLR.LambdaExpression> dlrMethods = ConvertNodes(methods);
+
+            AssemblyCreator asmCreator = new AssemblyCreator("Demo");
+            foreach (var item in dlrMethods)
+            {
+                asmCreator.AddMethod(item.Name.TrimStart('.'), item);
+            }
+
+            List<string> names = asmCreator.Build();
+            Console.WriteLine("Created '{0}.dll' with the following methods/properties/fields", asmCreator.AssemblyBaseName);
+            foreach (string name in names)
+	        {
+                Console.WriteLine("- {0}", name);
+	        }
+            Console.WriteLine("Number of generated entries: {0}", names.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
